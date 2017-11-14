dc716bb1154c1a33272023602b913fd8cb5cffd2
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-14 09:29:19 -0300

    add ingredient model file

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Ingredient } from "../../shared/ingredient.model";
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core'
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [];
 }
