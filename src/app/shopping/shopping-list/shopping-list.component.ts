c9b418437f26b92e301c8e3db42fbdf58c69cd86
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-14 09:43:54 -0300

    add shopping edit form

--- src/app/shopping/shopping-list/shopping-list.component.ts
+++ src/app/shopping/shopping-list/shopping-list.component.ts
@@ -7,5 +7,8 @@ import { Ingredient } from "../../shared/ingredient.model";
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-  ingredients: Ingredient[] = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
 }
