06097b36647dc2ec214098c5751f64927748260f
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-15 17:15:36 -0300

    add ingredients to recipe object

--- src/app/recipes/recipe-list/recipe.service.ts
+++ src/app/recipes/recipe-list/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "../recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../../shared/ingredient.model";
 
 export class RecipeService {
   onRecipeSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
@@ -7,9 +8,20 @@ export class RecipeService {
     // new Recipe('A test recipe 1', 'This is simply a test 1', 'http://www.seriouseats.com/recipes/assets_c/2015/01/20150119-pressure-cooker-chicken-stew-food-lab-11-thumb-1500xauto-418088.jpg'),
     // new Recipe('A test recipe 2', 'This is simply a test 2', 'http://www.seriouseats.com/recipes/assets_c/2015/01/20150119-pressure-cooker-chicken-stew-food-lab-11-thumb-1500xauto-418088.jpg'),
     // new Recipe('A test recipe 3', 'This is simply a test 3', 'http://www.seriouseats.com/recipes/assets_c/2015/01/20150119-pressure-cooker-chicken-stew-food-lab-11-thumb-1500xauto-418088.jpg')
-    new Recipe('A test recipe 1', 'This is simply a test 1', '../../../assets/tr.jpg'),
-    new Recipe('A test recipe 2', 'This is simply a test 2', '../../../assets/tr.jpg'),
-    new Recipe('A test recipe 3', 'This is simply a test 3', '../../../assets/tr.jpg')
+    new Recipe('Baião de 2', 'Baião é muito bom!', '../../../assets/tr.jpg',
+      [
+        new Ingredient("Arroz", 1),
+        new Ingredient("Feijão", 1),
+        new Ingredient("Queijo", 3)
+      ]
+    ),
+    new Recipe('Sauduíche de ovo', 'Fácil, rápido e barato', '../../../assets/tr.jpg', [
+      new Ingredient("Pão", 1),
+      new Ingredient("Ovo", 2),
+      new Ingredient("Orégano", 1),
+      new Ingredient("Sal", 1)
+    ]
+    )
   ];
 
   getRecipes = () => this.recipes.slice();
