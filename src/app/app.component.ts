37b377c1c8626a9ff38a87af84a81c3963d29197
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-20 19:20:46 -0300

    add something reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  somethingForm: FormGroup;
+  somethingArray = ['SomethingA', 'SomethingB', 'SomethingC'];
+  selectedSomething = 'SomethingB';
 
+  ngOnInit() {
+    this.somethingForm = new FormGroup({
+      'somethingData': new FormGroup({
+        'email': new FormControl(null, [Validators.required]),
+        'something': new FormControl(this.selectedSomething, [Validators.required])
+      })
+    });
+    this.somethingForm.patchValue({
+      'somethingData': {
+        'something': this.selectedSomething
+      }
+    });
+  }
+
+  onSubmit() {
+    console.log(this.somethingForm.value);
+  }
 }
