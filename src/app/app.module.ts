1711d3133d31b72a1142d24a03c902ee8388cd69
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-23 10:00:18 -0300

    clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from "./shopping/shopping-list/shopping-edit/shopping-edit.component";
-import { ShoppingEditTemplateDrivenComponent } from "./shopping/shopping-list/shopping-edit-td/shopping-edit-td.component";
-// import { ShoppingEditReactiveComponent } from "./shopping/shopping-list/shopping-edit-reactive/shopping-edit-reactive.component";
 import { ShoppingListService } from './shopping/shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe-list/recipe.service';
 import { RoutingModule } from './routing.module';
-import { RecipeResolver } from './recipes/recipe-list/recipe-resolver.service';
 import { HttpModule } from '@angular/http';
 import { DataStorageService } from './shared/data.storage.service';
-import { SiginComponent } from './auth/sigin/sigin.component';
-import { SigupComponent } from './auth/sigup/sigup.component';
 import { AuthService } from './auth/auth.service';
-import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping/shopping-list/shopping-list.module';
@@ -27,7 +18,6 @@ import { AuthModule } from './auth/auth.module';
   declarations: [
     AppComponent,
     HeaderComponent,
-    // ShoppingEditReactiveComponent,
   ],
   imports: [
     BrowserModule,
@@ -38,7 +28,7 @@ import { AuthModule } from './auth/auth.module';
     AuthModule,
     ShoppingListModule,
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
