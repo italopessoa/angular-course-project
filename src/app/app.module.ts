b6c36151c709decc4e19d07bb0bd91f33bcc1d7d
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-25 15:54:06 -0300

    register shopping list reducers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
 import { AuthGuard } from './auth/auth-guard.service';
 import { HttpClientModule } from "@angular/common/http";
+import { StoreModule } from '@ngrx/store';
+import { shopppingListReducer } from './shopping/shopping-list/store/shopping-list.reducers'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +25,7 @@ import { HttpClientModule } from "@angular/common/http";
     ShoppingListModule,
     CoreModule,
     HttpClientModule,
+    StoreModule.forRoot({ shoppingList: shopppingListReducer })
   ],
   bootstrap: [AppComponent]
 })
