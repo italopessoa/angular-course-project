61ae33e8413a591fbf907870bacc23557206bbdb
Italo Pessoa <italo.pessoa@hotmail.com>
2017-11-25 15:46:36 -0300

    add ingredients reducer and actions

---
+++ src/app/shopping/shopping-list/store/shopping-list.reducer.ts
@@ -0,0 +1,21 @@
+import { Action } from "@ngrx/store";
+import { Ingredient } from "../../../shared/ingredient.model";
+import * as ShoppingListActions from "./shopping-list.actions";
+
+const initialState = {
+  ingredients: [
+    new Ingredient('Maça', 2),
+    new Ingredient('Laranja', 10)
+  ]
+}
+export function shopppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+  switch (action.type) {
+    case ShoppingListActions.ADD_INGREDIENT:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      }
+    default:
+      return state;
+  }
+}
